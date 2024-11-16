20d91e20c74ca310cac9ac12ec3c0a96b325ec04
Nimira43 <n.rabone72@aol.com>
2024-11-16 07:48:33 +0000

    Setting up interface for repositories state and started writing reducer.

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,4 +1,18 @@
-const reducer = (state, action) => {
+interface RepositoriesState {
+  loading: boolean
+  error: string | null
+  data: string[]
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositories':
+    case 'search_repositories_success':
+    case 'search_repositories_error':
+    default:
+      return state
+  }
+
 
 }
 
