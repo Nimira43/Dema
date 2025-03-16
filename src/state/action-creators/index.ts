fd7b36eee6a7400c92b1e34c923d7b2ba0c7fe4e
Nimira43 <n.rabone72@aol.com>
2025-03-16 16:54:08 +0000

    Foundation for an asynchronous action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,3 +2,8 @@ import axios from 'axios'
 import { ActionType } from '../action-types'
 import { Action } from '../actions'
 
+const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    
+  }
+}
