f1a98818c6d83a6aa6557713b9cdd433bf230808
Nimira43 <n.rabone72@aol.com>
2025-03-16 16:59:10 +0000

    Error handling

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -7,5 +7,15 @@ const searchRepositories = (term: string) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     })
+    try {
+
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.SEARCH_REPOSITORIES_ERROR,
+          payload: err.message
+        })
+      }
+    }
   }
 }
