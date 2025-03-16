4da0634544c5d937bcdca44631317b074bc66201
Nimira43 <n.rabone72@aol.com>
2025-03-16 06:10:59 +0000

    Copied and adapted the Components React project to create React boilerplate for this one.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='logo'>Déma</h1>
+    </div>
+  )
+}
+
+export default App
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>NPM Package Searcher</h1>
-    </div>
-  )
-}
-
-export default App
