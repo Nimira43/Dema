1958b27ba9eba0dee9eb96aed381c9ba049172f9
Nimira43 <n.rabone72@aol.com>
2025-03-16 16:33:09 +0000

    Placed ActionType enum into action-types/index.ts.

--- src/state/reducers/repositioriesReducers.ts
+++ src/state/reducers/repositioriesReducers.ts
@@ -5,11 +5,7 @@ interface RepositoriesState {
 }
 
 
-enum ActionType {
-  SEARCH_REPOSITORIES = 'search_repositories',
-  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
-}
+
   
 const reducer = (
   state: RepositoriesState,
