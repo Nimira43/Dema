a042342458945d3e51cd316ecca4d6224d4a3b9b
Nimira43 <n.rabone72@aol.com>
2025-05-08 22:17:34 +0100

    Created components/RepositoriesList.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from './state'
+import RepositoriesList from './components/RepositoriesList'
 
 function App() {
   return (
---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RepositoriesList() {
+  return (
+    <div>
+      <h1>Respositories List</h1>
+    </div>
+  )
+}
+
+export default RepositoriesList
