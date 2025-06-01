c5a02739d99a13e0ccd5c701107c3f693a859202
Nimira43 <n.rabone72@aol.com>
2025-06-01 04:32:49 +0100

    Qick tidy up. Project completed.

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -20,13 +20,13 @@ const RepositoriesList = () => {
           value={term}
           onChange={e => setTerm(e.target.value)}
         />
-          <button>Search</button>
-        </form>
-        {error && <h3>{error}</h3>}
-        {loading && <h3>Loading...</h3>}
-        {!error && !loading && 
-          data.map((name) => <div key={name}>{name}</div>)
-        }
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+        data.map((name) => <div key={name}>{name}</div>)
+      }
     </div>
   )
 }
